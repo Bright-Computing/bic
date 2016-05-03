1e9ba2150aad54c071cfc77fbff873b1926902bb
Johnny Devaprasad <johnny.devaprasad@brightcomputing.com>
2016-05-03 20:01:24 +0200

    reverse conditionals when checking for LMOD_SYSTEM_DEFAULT_MODULES

--- Lmod/branches/7.0/z01-default_modules.csh
+++ Lmod/branches/7.0/z01-default_modules.csh
@@ -1,7 +1,9 @@
 if ( ! $?__Init_Default_Modules || ! $?LD_LIBRARY_PATH )  then
-  if ( -f "/usr/share/modulefiles/DefaultModules.lua" ) then
-    if ( ! $?LMOD_SYSTEM_DEFAULT_MODULES ) then
+  if ( ! $?LMOD_SYSTEM_DEFAULT_MODULES ) then
+    if ( -f "/usr/share/modulefiles/DefaultModules.lua" ) then
       setenv LMOD_SYSTEM_DEFAULT_MODULES "DefaultModules"
+    else
+      setenv LMOD_SYSTEM_DEFAULT_MODULES "StdEnv"
     endif
   endif
   module --initial_load restore
--- Lmod/branches/7.0/z01-default_modules.sh
+++ Lmod/branches/7.0/z01-default_modules.sh
@@ -1,8 +1,10 @@
 if [ -z "$__Init_Default_Modules" -o -z "$LD_LIBRARY_PATH" ]; then
    export __Init_Default_Modules=1;
-   if [ -f "/usr/share/modulefiles/DefaultModules.lua" ];then
-     if [ -z "$LMOD_SYSTEM_DEFAULT_MODULES" ];then
+   if [ -z "$LMOD_SYSTEM_DEFAULT_MODULES" ];then
+     if [ -f "/usr/share/modulefiles/DefaultModules.lua" ];then
        export LMOD_SYSTEM_DEFAULT_MODULES="DefaultModules"
+     else
+       export LMOD_SYSTEM_DEFAULT_MODULES="StdEnv"
      fi
    fi
    module --initial_load restore
