27ec966a1989657dfe3d49b18dc283e0081b7f46
Johnny Devaprasad <sendmail.jd@gmai..com>
2016-10-14 04:29:41 -0700

    Update to Lmod 6.6. Re-initialize default modules only if USER_IS_ROOT is set.

--- Lmod/branches/7.0/z01-default_modules.sh
+++ Lmod/branches/7.0/z01-default_modules.sh
@@ -1,15 +1,17 @@
-if [ -z "$__Init_Default_Modules" ]; then
-   export __Init_Default_Modules=1;
-   if [ -z "$LMOD_SYSTEM_DEFAULT_MODULES" ];then
-     if [ -f "/usr/share/modulefiles/DefaultModules.lua" ];then
-       export LMOD_SYSTEM_DEFAULT_MODULES="DefaultModules"
-     else
-       if [ -f "/usr/share/modulefiles/StdEnv.lua" ];then
-         export LMOD_SYSTEM_DEFAULT_MODULES="StdEnv"
+if [[ -n "$USER_IS_ROOT" ]];then
+  if [ -z "$__Init_Default_Modules" ]; then
+     export __Init_Default_Modules=1;
+     if [ -z "$LMOD_SYSTEM_DEFAULT_MODULES" ];then
+       if [ -f "/usr/share/modulefiles/DefaultModules.lua" ];then
+	 export LMOD_SYSTEM_DEFAULT_MODULES="DefaultModules"
+       else
+	 if [ -f "/usr/share/modulefiles/StdEnv.lua" ];then
+	   export LMOD_SYSTEM_DEFAULT_MODULES="StdEnv"
+	 fi
        fi
      fi
-   fi
-   module --initial_load restore
-else
-   module refresh
+     module --initial_load restore
+  else
+     module refresh
+  fi
 fi
