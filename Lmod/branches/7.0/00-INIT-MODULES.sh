2e2b80316c740a6b96ce410fdf73abe9d33ad118
Fotis Georgatos <kefalonia@gmail.com>
2016-10-26 03:40:16 +0100

    cluster modulefile simplifies configuration

--- Lmod/branches/7.0/00-INIT-MODULES.sh
+++ Lmod/branches/7.0/00-INIT-MODULES.sh
@@ -4,15 +4,9 @@ if [ -z "$MODULEPATH" ]; then
 
   ## Initializations
   export LMOD_RC=/etc/site/extras/lmodrc.lua
-  #export LMOD_SYSTEM_NAME=AVX2
-  #export LMOD_SYSTEM_DEFAULT_MODULES="HPCBIOS/2015q2 sge"
-  export LMOD_SYSTEM_DEFAULT_MODULES="sge"
+  ##export LMOD_SYSTEM_NAME=AVX2
+  ##export LMOD_SYSTEM_DEFAULT_MODULES="HPCBIOS/2015q2 sge"
+  export LMOD_SYSTEM_DEFAULT_MODULES="sge cluster"
   export LMOD_PACKAGE_PATH=/etc/site/lmod
 
-  ## help EasyBuild, too
-  export EASYBUILD_BUILDPATH=/dev/shm
-  export EASYBUILD_MODULES_TOOL=Lmod
-
 fi
-
-
