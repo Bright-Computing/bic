d2a0f43c4e8dd7b487d16cf2319f6b4ebdabf418
Fotis Georgatos <kefalonia@gmail.com>
2017-06-20 02:11:39 +0200

    improve Lmod-illumina-prepare.sh

--- Lmod-illumina-prepare.sh
+++ Lmod-illumina-prepare.sh
@@ -14,15 +14,16 @@ CMVERSION=7.0
 export CWD=`pwd`
 cd $CWD
 
-TMPDIR=/tmp/$NAME-build-$RANDOM
+TMPDIR=/tmp/$NAME-build-$RANDOM-$$
 
 cp *.patch $RPMDIR/
 
 mkdir -p $TMPDIR/$NAME-$CMVERSION
-
 cp *.sh $TMPDIR/$NAME-$CMVERSION
 cp *.csh $TMPDIR/$NAME-$CMVERSION
+cp *.lua $TMPDIR/$NAME-$CMVERSION
 
 cd $TMPDIR
 tar -zcvf $RPMDIR/$NAME-$CMVERSION.tar.gz $NAME-$CMVERSION
+echo $RPMDIR/$NAME-$CMVERSION.tar.gz
 rm -rf $TMPDIR
