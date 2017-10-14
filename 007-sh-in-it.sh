fb0c91cc054806cec7a692caff7b389a40c210da
Fotis Georgatos <kefalonia@gmail.com>
2017-10-14 12:13:37 +0200

    add headers

--- 007-sh-in-it.sh
+++ 007-sh-in-it.sh
@@ -1,3 +1,5 @@
+#### Why:  Initiate variable settings in Unix/Linux environments via definitions in YAML, fi. for default $MODULEPATH, LMOD_*, EASYBUILD_* etc
+#### Who:  Fotis Georgatos, 2017, MIT license
 PARSEFILES=$(ls /etc/profile.definitions/{global*,site/*,nodecategory/*,groups/`id -gn`,user/`id -un`}.yml 2>/dev/null)
 
 if [ -z "$__Init_Default_Profile" ]; then
