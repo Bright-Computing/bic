12b32363df7d21c5f6463753eba7617572c093a5
Fotis Georgatos <kefalonia@gmail.com>
2017-10-18 12:18:15 +0200

    why not make safer .sh script as well?

--- 007-sh-in-it.sh
+++ 007-sh-in-it.sh
@@ -3,7 +3,7 @@
 PARSEFILES=$(ls /etc/profile.definitions/{global*,site/*,nodecategory/*,groups/`id -gn`,user/`id -un`}.yml 2>/dev/null)
 
 if [ -z "$__Init_Default_Profile" ]; then
-  eval `for i in $PARSEFILES; do python /etc/profile.d/007-sh-in-it.xyzzy.py $i;done`
+  eval `for i in $PARSEFILES; do [[ -f $i ]] && python /etc/profile.d/007-sh-in-it.xyzzy.py $i;done`
   export __Init_Default_Profile=1;
 fi
 
