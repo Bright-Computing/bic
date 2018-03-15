3f6b724f688c7da0e726fb79e619a061ebefb63d
Fotis Georgatos <kefalonia@gmail.com>
2018-03-15 12:25:43 +0000

    Upgrade cm8.0 tag and reduce clutter in the script

--- Lmod-illumina-prepare.sh
+++ Lmod-illumina-prepare.sh
@@ -3,27 +3,17 @@
 export CWD=`pwd`
 
 RPMDIR=$HOME/rpmbuild/SOURCES
-if [ -d /usr/src/redhat ]
- then if [ ! -x $RPMDIR ]
-  then export RPMDIR=/usr/src/redhat/SOURCES
- fi
-fi
+mkdir -p $RPMDIR
 
 NAME=Lmod-files
-CMVERSION=7.0
+CMVERSION=8.0
 
 export CWD=`pwd`
 cd $CWD
-
 TMPDIR=/tmp/$NAME-build-$RANDOM-$$
-
 cp *.patch $RPMDIR/
-
 mkdir -p $TMPDIR/$NAME-$CMVERSION
-cp *.sh $TMPDIR/$NAME-$CMVERSION
-cp *.csh $TMPDIR/$NAME-$CMVERSION
-cp *.lua $TMPDIR/$NAME-$CMVERSION
-cp *.py $TMPDIR/$NAME-$CMVERSION
+cp *.sh *.csh *.lua *.py $TMPDIR/$NAME-$CMVERSION
 
 cd $TMPDIR
 tar -zcvf $RPMDIR/$NAME-$CMVERSION.tar.gz $NAME-$CMVERSION
